21debf53dffbddd59d9c690e2d7b5b5930861397
Danny Lin <danny.l@ljit.io>
2018-07-31 10:31:44 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import {
+	Text,
+	View,
+} from "react-native";
+
+class App extends Component {
+	render() {
+		return (
+			<Text>GridView</Text>
+		);
+	}
+}
+
+export default App;
